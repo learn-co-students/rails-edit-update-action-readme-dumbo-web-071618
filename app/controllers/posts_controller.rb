c11faa0cd007adca961ac5c6b32eee6505c49e7e
Tokuhisa1 <TokuhisaWinston@Outlook.com>
2018-08-11 13:36:13 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,4 +18,21 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		# raise params.inspect
+		# Query the database for the Post record that matches the :id passed to the
+		# route.
+		# Store the query in an instance variable.
+		@post = Post.find(params[:id])
+		# Update the values passed from the form
+		# Save the changes in the database.
+		@post.update(title: params[:title], description: params[:description])
+		# Redirect the user to the show page so they can see the updated record.
+		redirect_to post_path(@post)
+	end
 end
